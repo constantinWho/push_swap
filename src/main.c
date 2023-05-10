a591f589c5ec6d76d67350fedad44b85af31323d
chustei <chustei@student.42berlin.de>
2023-05-10 17:56:58 +0200

    sorting in bigger chunks

--- src/main.c
+++ src/main.c
@@ -6,19 +6,96 @@
 /*   By: chustei <chustei@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 13:22:23 by chustei           #+#    #+#             */
-/*   Updated: 2023/05/08 13:39:21 by chustei          ###   ########.fr       */
+/*   Updated: 2023/05/10 16:41:15 by chustei          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
+int	ft_partition(int *stack, int low, int high)
+{
+	int	pivot;
+	int	i;
+	int	j;
+	int	temp;
+
+	pivot = stack[high];
+	i = low - 1;
+	j = low;
+	while (j < high)
+	{
+		if (stack[j] < pivot)
+		{
+			i++;
+			temp = stack[i];
+			stack[i] = stack[j];
+			stack[j] = temp;
+		}
+		j++;
+	}
+	temp = stack[i + 1];
+	stack[i + 1] = stack[high];
+	stack[high] = temp;
+	return (i + 1);
+}
+
+void	ft_quick_sort(int *stack, int low, int high)
+{
+	int	pivot;
+
+	if (low < high)
+	{
+		pivot = ft_partition(stack, low, high);
+		ft_quick_sort(stack, low, pivot - 1);
+		ft_quick_sort(stack, pivot + 1, high);
+	}
+}
+
+int	*ft_stack_copy(int *stack, int size)
+{
+	int	i;
+	int	*stack_copy;
+
+	i = 0;
+	stack_copy = malloc(sizeof(int) * size);
+	while (i < size)
+	{
+		stack_copy[i] = stack[i];
+		i++;
+	}
+	return (stack_copy);
+}
+
 void	ft_store_stack_a(char **av, t_stack *stack)
 {
 	int	i;
+	int	j;
+	int	*stack_copy;
 
 	i = 0;
 	while (av[++i])
 		stack->a[i - 1] = ft_atoi(av[i]);
+	stack_copy = ft_stack_copy(stack->a, stack->size_a);
+	ft_quick_sort(stack_copy, 0, stack->size_a - 1);
+/* 	i = -1;
+	ft_printf("stack_a: ");
+	while (++i < stack->size_a)
+		ft_printf("%i ", stack->a[i]); */
+	i = -1;
+	while (++i < stack->size_a)
+	{
+		j = -1;
+		while (++j < stack->size_a)
+		{
+			if (stack->a[i] == stack_copy[j])
+			{
+				stack->a[i] = j;
+				break ;
+			}
+		}
+	}
+/* 	ft_printf("\n"); */
+	free(stack_copy);
 }
 
 void	ft_print_output(t_stack	*stack)
--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: chustei <chustei@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 11:03:53 by chustei           #+#    #+#             */
-/*   Updated: 2023/05/08 15:00:41 by chustei          ###   ########.fr       */
+/*   Updated: 2023/05/10 17:26:12 by chustei          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,45 +66,105 @@ void	ft_sort_three(t_stack *stack)
 		ft_rotate(stack->a, 2, stack->size_a, "rra");
 }
 
-void	ft_push_sort(t_stack *stack, int pivot)
+void	ft_sort_five(t_stack *stack)
 {
-	int	i;
-
-	i = -1;
-	ft_printf("pivot: %d stack_b size: %d\n", pivot, stack->size_b);
-/* 	while (++i < stack->size_b)
-	{
-		ft_push_a(stack);
-		if (stack->a[i] > pivot)
-			ft_rotate(stack->a, i, stack->size_a, "ra");
-	} */
-}
-
-/* void	ft_push_swap(t_stack *stack)
-{
-	int	pivot;
 	int	i;
 	int	j;
+	int	min_pos;
 
-	j = stack->size_a;
 	i = -1;
-	pivot = stack->a[stack->size_a - 1];
+	j = stack->size_a;
 	while (++i < j - 1)
 	{
-		ft_printf("pivot: %d stack_a[%d]: %d\n", pivot, 0, stack->a[0]);
-		if (stack->a[0] < pivot)
+		min_pos = ft_min_pos(stack->a, stack->size_a);
+		if (min_pos == 0)
 			ft_push_b(stack);
-		else
-			ft_rotate(stack->a, i, stack->size_a, "ra");
+		else if (min_pos == 1)
+			ft_rotate(stack->a, 1, stack->size_a, "ra");
+		else if (min_pos == 2)
+			ft_rotate(stack->a, 2, stack->size_a, "ra");
+		else if (min_pos == 3)
+			ft_rotate(stack->a, 3, stack->size_a, "ra");
+		else if (min_pos == 4)
+			ft_rotate(stack->a, 4, stack->size_a, "ra");
 	}
-	if (ft_check_sorted(stack->a, stack->size_a) == 1)
-		ft_push_swap(stack);
-}*/
+	ft_sort_three(stack);
+	ft_push_a(stack);
+	ft_push_a(stack);
+}
+
+void	ft_sort(t_stack *stack)
+{
+	int	max;
+	int	i;
+	int	j;
+	int	ratio;
+
+	max = stack->size_a - 1;
+	ratio = 10;
+	ft_printf("max: %d\n", max);
+	i = -1;
+	while (++i < max + 1)
+	{
+		j = -1;
+		while (++j < max + 1)
+		{
+			if (stack->a[j] < ratio && stack->a[j] != max && \
+			stack->a[j] != max - 1 && stack->a[j] != max - 2 && \
+			stack->a[j] != max - 3 && stack->a[j] != max - 4)
+			{
+				if (j == 0)
+				{
+					ft_push_b(stack);
+/* 					ft_rotate(stack->b, j, stack->size_b, "rb"); */
+				}
+				if (max - j > max / 2)
+					ft_rotate(stack->a, j, stack->size_a, "ra");
+				else
+					ft_rotate(stack->a, j, stack->size_a, "rra");
+				break ;
+			}
+		}
+	}
+/* 	ft_push_swap(stack);
+	if (stack->size_b > 0)
+		ft_push_a(stack); */
+}
 
 void	ft_push_swap(t_stack *stack)
 {
+	if (stack->a[0] > stack->a[1] && stack->size_a == 2)
+		ft_swap(&stack->a[0], &stack->a[1]);
+/* 	ft_printf("size_a: %d\n", stack->size_a); */
+	if (stack->size_a == 3)
+		ft_sort_three(stack);
+	if (stack->size_a == 5)
+		ft_sort_five(stack);
+	if (stack->size_a > 5)
+		ft_sort(stack);
+/* 	ft_printf("size_a: %d\n", stack->size_a); */
+}
+
+/*
+
+void	ft_quick_sort(int *stack, int low, int high)
+{
+	int	pivot;
+
+	if (low < high)
+	{
+		pivot = ft_partition(stack, low, high);
+		ft_quick_sort(stack, low, pivot - 1);
+		ft_quick_sort(stack, pivot + 1, high);
+	}
+} */
+
+/* void	ft_push_swap(t_stack *stack)
+{
 	int	min_pos;
 
+	if (stack->a[0] < stack->a[1])
+		ft_swap(&stack->a[0], &stack->a[1]);
 	if (stack->size_a == 3)
 		ft_sort_three(stack);
 	else if (stack->size_a > 1)
@@ -121,8 +181,7 @@ void	ft_push_swap(t_stack *stack)
 		if (stack->size_b > 0)
 			ft_push_a(stack);
 	}
-}
-
+} */
 
 /* #include <stdio.h>
 
